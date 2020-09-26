2e8f85fea9a29709813a4009a6509b0997b4b272
WinnieVenice <2602306472@qq.com>
2020-09-27 02:25:29 +0800

    9.16

---
+++ 大数/model BigInt （未完成.cpp
@@ -0,0 +1,136 @@
+#include<bits/stdc++.h>
+#define LL __int128
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const int mod=7+1e9;
+ll gcd(ll x,ll y){return y?gcd(y,x%y):x;}
+ll lcm(ll x,ll y){return x/gcd(x,y)*y;}
+ll qpow(ll a,ll b,ll p){a%=p; ll ret=1;for(;b;b>>=1,a=a*a%p) if(b&1) ret=ret*a%p; return ret;}
+ll qpow(ll a,ll b){ll ret=1;for(;b;b>>=1,a=a*a) if(b&1) ret=ret*a; return ret;}
+ll getInv(ll x,ll p){return qpow(x,p-2,p);}
+class BigInt{
+    public:
+        string v;
+        int len;
+        bool Sign;
+        inline BigInt(){v.clear(); len=0; Sign=0;}
+        inline BigInt(string s){
+            v.clear();
+            Sign=0;
+            if(*s.begin()=='-') Sign=1,s.erase(s.begin());
+            reverse(s.begin(),s.end());
+            v=s;
+            len=(int)v.length();
+        }
+        inline BigInt(long long x){
+            v.clear();
+            Sign=0;
+            if(x<0) Sign=1,x=-x; 
+            for(;x;x/=10) v+=x%10+'0';
+            len=(int)v.length();
+        }
+        inline BigInt(const BigInt &x){
+            v=x.v;
+            len=x.len;
+            Sign=x.Sign;
+        }
+        inline void init(long long x){
+            v.clear();
+            Sign=0;
+            if(x<0) Sign=1,x=-x;
+            for(;x;x/=10) v+=x%10+'0';
+            len=(int)v.length();
+        }
+        inline void operator=(const BigInt &x){
+            v=x.v;
+            len=x.len;
+            Sign=x.Sign;
+        }
+        inline void operator=(const long long &x){ init(x);}
+        inline bool operator>(const BigInt &x){
+            if(x.Sign!=Sign) return Sign>x.Sign;
+            if(Sign<0) return (*this<x);
+            if(x.len!=len) return len>x.len;
+            for(int i=len-1;i>=0;i--)
+                if(x.v[i]!=v[i]) return v[i]>x.v[i];
+            return 0;
+        }
+        inline bool operator<(const BigInt &x){
+            if(x.Sign!=Sign) return Sign<x.Sign;
+            if(Sign<0) return (*this>x);
+            if(x.len!=len) return len>x.len;
+            for(int i=len-1;i>=0;i--)
+                if(x.v[i]!=v[i]) return v[i]<x.v[i];
+            return 0;
+        }
+        inline bool operator==(const BigInt &x){
+            if(x.Sign!=Sign) return 0;
+            if(x.len!=len) return 0;
+            for(int i=0;i<len;i++)
+                if(x.v[i]!=v[i]) return 0;
+            return 1;
+        }
+        friend BigInt max(BigInt x,BigInt y){
+            if(x>y) return x;
+            else return y;   
+        }
+        friend BigInt min(BigInt x,BigInt y){
+            if(x<y) return x;
+            else return y;
+        }
+        friend BigInt operator+(const BigInt &x,const BigInt &y){
+            if(x.Sign!=y.Sign) return x-y;
+            string s;
+            int len=std::min(x.len,y.len);
+            int j=0;
+            for(int i=0;i<len;i++){
+                int cur=x.v[i]+y.v[i]-2*'0';
+                s+=(cur+j)%10+'0';
+                j=(cur+j)/10;
+            }
+            BigInt mx(max(x,y));
+            for(int i=len;i<mx.len;i++){
+                s+=(mx.v[i]-'0'+j)%10+'0';
+                j=(mx.v[i]-'0'+j)/10;
+            }
+            for(;j;j/=10) s+=j%10+'0';
+            reverse(s.begin(),s.end());
+            return BigInt(s);
+        }
+        friend BigInt operator-(const BigInt &x,const BigInt &y){
+            if(x.Sign!=y.Sign) return x+y;
+            string s;
+            int len=std::min(x.len,y.len);
+            int j=0;
+            for(int i=0;i<len;i++){
+                if(x.v[i]-j>=y.v[i]) s+=x.v[i]-j-y.v[i]+'0',j=0;
+                else s+=10+x.v[i]-j-y.v[i]+'0',j++;
+            }
+            BigInt mx(max(x,y));
+            for(int i=len;i<mx.len;i++){
+                s+=(mx.v[i]-'0'+j)%10+'0';
+                j=(mx.v[i]-'0'+j)/10;
+            }
+            reverse(s.begin(),s.end());
+            return BigInt(s);
+        }
+        friend void print(const BigInt &x){ for(int i=x.len-1;i>=0;i--) cout<<x.v[i];}
+        friend ostream &operator<<(ostream &cout,const BigInt &x){ print(x); return cout;}
+        friend istream &operator>>(istream &cin,BigInt &x){
+            x.v.clear();
+            cin>>x.v;
+            reverse(x.v.begin(),x.v.end());
+            x.len=(int)x.v.length();
+            return cin;
+        }
+};
+
+void solve(){
+    BigInt a,b,c,d; cin>>a>>b>>c>>d;
+    cout<<(a+b+c+d)<<'\n';
+}
+int main(){
+    int T; cin>>T;
+    while(T--) solve();
+}

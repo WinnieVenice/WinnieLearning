e20952f39ab21fe92f48ede9f67b7ba5b3cdbc8b
WinnieVenice <62410590+WinnieVenice@users.noreply.github.com>
2021-09-19 03:21:40 +0800

    2021.9.19

--- 线性代数/model_gauss.cpp
+++ 线性代数/model_gauss.cpp
@@ -10,53 +10,7 @@ pair<int,int> exgcd(int a,int b) { if(!b) return {1, 0}; pair<int,int> ret = exg
 namespace gauss {
     const int N = 5 + 6e2;
     const double eps = 1e-6;
-    void swap()
     int gauss(double a[][N], int n, int m) {    //n = row, m = col
-        int now_r = 0;
-        for (int i = 0; i < m - 1 && now_r < n; i++, now_r++) {
-            int mx = now_r;
-            for (int j = now_r; j < n; j++) {
-                if (fabs(a[mx][i]) < fabs(a[j][i])) {
-                    mx = j;
-                }
-            }
-            if (mx != now_r) {
-                for (int j = i; j < m; j++) {
-                    swap(a[now_r][j], a[mx][j]);
-                }
-            } 
-            if (fabs(a[now_r][i]) < eps) {
-                now_r--;
-                continue;
-            }
-            for (int j = now_r + 1; j < n; j++) {
-                if (fabs(a[j][i]) < eps) {
-                    continue;
-                }
-                double v = a[j][i] / a[now_r][i];
-                for (int k = i; k < m; k++) {
-                    a[j][k] -= a[now_r][k] * v;
-                }
-            }
-        }
-        for (int i = now_r; i < n; i++) {
-            if (fabs(a[i][m - 1]) > eps) {
-                return -1;
-            } 
-        }
-        if (now_r < n) {
-            return n - now_r; 
-        }
-        for (int i = now_r - 1; i >= 0; i--) {
-            a[i][m - 1] /= a[i][i];
-            for (int k = 0; k < i; k++) {
-                a[k][m - 1] -= a[k][i] * a[i][m - 1];
-            }
-        }   
-        return 0;
-    }
-    int gauss(vector<vector<double>> &a) {
-        int n = a.size(), m = a[0].size();
         int now_r = 0;
         for (int i = 0; i < m - 1 && now_r < n; i++, now_r++) {
             int mx = now_r;

d4683c1909e464685dec529c9cf72872d1ec9bc6
WinnieVenice <62410590+WinnieVenice@users.noreply.github.com>
2021-12-07 20:42:12 +0800

    2021.12.7

--- leetcode/753.破解保险箱.cpp
+++ leetcode/753.破解保险箱.cpp
@@ -11,9 +11,7 @@ public:
     map<string, int> mp;
     map<int, string> id;
     vector<int> G[100005];
-    int _cnt1 = 0;
     void dfs(int x, string u, int n, int k) {
-        if (++_cnt1>30000) return;
         if (x >= n) {
             if (!mp[u]) {
                 mp[u] = ++tot;
@@ -27,9 +25,7 @@ public:
     }
     int cnt;
     int pt[100005];
-    int _cnt = 0;
     void dfs1(int x) {
-        if (_cnt++ > 10000) return;
         for (; !G[x].empty(); ) {
             auto u = G[x].back(); G[x].pop_back();
             dfs1(u);
@@ -37,22 +33,24 @@ public:
         pt[++cnt] = x;
     }
     string crackSafe(int n, int k) {
-        dfs(0, "", n, k);
+        if (n == 1) {
+            string ans = "";
+            for (int i = 0; i < k; i++) ans += char(i + '0');
+            return ans;
+        }
+        dfs(0, "", n - 1, k);
         for (auto &u : mp) {
             string v = u.first.substr(1, u.first.size() - 1);
-            for (char ch = '0'; ch <= '9'; ch++) {
-                v += ch;
-                G[u.second].push_back(mp[v]);
-            }    
+            for (int i = 0; i < k; i++) {
+                G[u.second].push_back(mp[v + char(i + '0')]);
+            }
         }
-        puts("~");
         dfs1(1);
         string ans = id[pt[cnt]];
         for (int i = cnt - 1; i >= 1; i--) {
-            ans += id[pt[cnt]][n - 1];
+            ans += id[pt[i]][n - 2];
         }
         return ans;
     }
 };
 // @lc code=end
-

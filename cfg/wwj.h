520291a9c349ca19cc65bcc79da7cdad29680e55
WinnieVenice <62410590+WinnieVenice@users.noreply.github.com>
2022-03-03 13:34:48 +0800

    2022.3.3

---
+++ cfg/wwj.h
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+namespace discrete {
+    #define T int
+    vector<T> a;
+    int n;
+    void init(T inf) { a.clear(); a.emplace_back(-inf); a.emplace_back(inf); }
+    void add(T x) { a.emplace_back(x); }
+    void work() {
+        sort(a.begin(), a.end());
+        n = unique(a.begin(), a.end()) - a.begin();
+    }
+    void era() { a.erase(a.begin() + n, a.end()); }
+    int get(T x, int y) {
+        int u = 0;
+        if (y == 0) { // 找第一个a_i <= x
+            u = upper_bound(a.begin(), a.begin() + n, x) - a.begin();
+            u--;
+        } else if (y == 1) { // 找第一个a_i >= x
+            u = lower_bound(a.begin(), a.begin() + n, x) - a.begin();
+        } else if (y == 2) { // 找第一个a_i > x
+            u = upper_bound(a.begin(), a.begin() + n, x) - a.begin();
+        } else { //找第一个a_i < x
+            u = lower_bound(a.begin(), a.end(), x) - a.begin();
+            u++;
+        }
+        return u;
+    }
+    #undef T
+}
